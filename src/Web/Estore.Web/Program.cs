61f2a330682301cc5d434c39d83fa71ff2399203
Viktor-Asenov <79524317+Viktor-Asenov@users.noreply.github.com>
2022-04-23 17:08:56 +0300

    Publish option add

--- src/Web/Estore.Web/Program.cs
+++ src/Web/Estore.Web/Program.cs
@@ -15,6 +15,7 @@
                 .ConfigureWebHostDefaults(webBuilder =>
                     {
                         webBuilder.UseStartup<Startup>();
+                        webBuilder.UseIIS();
                     });
     }
 }
